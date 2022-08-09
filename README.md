d2ef48f3a414c39179ba510a4de52427fa3e94cb
jhansiravula <jhansiravula33@gmail.com>
2022-08-09 21:56:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# condi-control-setvalidators-svsx75
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/condi-control-setvalidators-svsx75)
---
+++ src/app/app.component.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      // optionA: new FormControl(false),
+      // optionB: new FormControl(false),
+      // optionBExtra: new FormControl(''),
+      // optionCExtra: new FormControl(''),
+      text1: new FormControl(''),
+      text2: new FormControl('')
+    });
+  }
+  //   this.optionB.valueChanges.subscribe((checked) => {
+  //     if (checked) {
+  //       this.optionBExtra.setValidators([
+  //         Validators.required,
+  //         Validators.minLength(5),
+  //         Validators.maxLength(15),
+  //       ]);
+  //       this.optionCExtra.setValidators([
+  //         Validators.required,
+  //         Validators.minLength(5),
+  //         Validators.maxLength(15),
+  //       ]);
+  //     } else {
+  //       this.optionBExtra.setValidators(null);
+  //       this.optionCExtra.setValidators(null);
+  //       this.optionBExtra.setValue('');
+  //       this.optionCExtra.setValue('');
+  //     }
+  //     this.optionBExtra.updateValueAndValidity();
+  //   });
+
+  //   this.optionA.valueChanges.subscribe((checked) => {
+  //     if (checked) {
+  //       this.optionCExtra.setValidators([
+  //         Validators.required,
+  //         Validators.minLength(5),
+  //         Validators.maxLength(10),
+  //       ]);
+  //     } else {
+  //       this.optionCExtra.setValidators(null);
+  //       this.optionCExtra.setValue('');
+  //     }
+  //     this.optionCExtra.updateValueAndValidity();
+  //   });
+  // }
+
+  // get optionA() {
+  //   return this.form.get('optionA') as FormControl;
+  // }
+
+  // get optionB() {
+  //   return this.form.get('optionB') as FormControl;
+  // }
+
+  // get optionBExtra() {
+  //   return this.form.get('optionBExtra') as FormControl;
+  // }
+
+  // get optionCExtra() {
+  //   return this.form.get('optionCExtra') as FormControl;
+  // }
+
+  // keyPressNumbers(event) {
+  //   var charCode = (event.which) ? event.which : event.keyCode;
+  //   // Only Numbers 0-9
+  //   if ((charCode < 48 || charCode > 57)) {
+  //     event.preventDefault();
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+
+  save() {
+
+  }
+}
